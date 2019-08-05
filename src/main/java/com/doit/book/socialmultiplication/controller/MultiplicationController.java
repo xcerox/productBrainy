6a5814bfc86828607fccb69897bf1074827bae16
Jairo Reyes Rosa <jareyes@nt.core>
2019-08-05 14:49:38 -0400

    adding presentation layer(rest api)

---
+++ src/main/java/com/doit/book/socialmultiplication/controller/MultiplicationController.java
@@ -0,0 +1,27 @@
+package com.doit.book.socialmultiplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.doit.book.socialmultiplication.domain.Multiplication;
+import com.doit.book.socialmultiplication.service.MultiplicationService;
+
+@RestController
+@RequestMapping("/multiplications")
+public class MultiplicationController {
+	
+	private final MultiplicationService multiplicationService;
+	
+	@Autowired
+	public MultiplicationController(final MultiplicationService multiplicationService) {
+		this.multiplicationService = multiplicationService;
+	}
+	
+	@GetMapping("/random")
+	Multiplication getRandomMultiplication() {
+		return multiplicationService.createRandomMultiplication();
+	}
+
+}
